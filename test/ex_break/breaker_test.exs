e069322a6d9cb3243ea78ce869a18ac10934626f
Jonathan Clem <jclem@github.com>
2019-07-10 07:42:48 -0500

    Add more Breaker and Registry tests

--- test/ex_break/breaker_test.exs
+++ test/ex_break/breaker_test.exs
@@ -9,6 +9,25 @@ defmodule ExBreak.BreakerTest do
     %{breaker: pid}
   end
 
+  describe ".increment/2" do
+    test "increments the breaker's tripped count", %{breaker: pid} do
+      Breaker.increment(pid, 0)
+      assert Agent.get(pid, & &1.break_count) == 1
+    end
+
+    test "set no tripped and tripped_at if count does not exceed threshold", %{breaker: pid} do
+      Breaker.increment(pid, 2)
+      assert Agent.get(pid, & &1) == %Breaker{break_count: 1, tripped: false, tripped_at: nil}
+    end
+
+    test "sets tripped and tripped_at if count exceeds threshold", %{breaker: pid} do
+      Breaker.increment(pid, 1)
+      breaker = Agent.get(pid, & &1)
+      assert breaker.tripped
+      assert breaker.tripped_at
+    end
+  end
+
   describe ".is_tripped/2" do
     test "returns false if the breaker is not tripped", %{breaker: pid} do
       Agent.update(pid, &Map.put(&1, :tripped, false))
@@ -37,6 +56,25 @@ defmodule ExBreak.BreakerTest do
       )
 
       assert Breaker.is_tripped(pid, 70)
+    end
+  end
+
+  describe ".reset_tripped/1" do
+    test "resets the breaker if it is tripped", %{breaker: pid} do
+      Breaker.increment(pid, 1)
+      breaker = Agent.get(pid, & &1)
+      assert breaker.tripped
+      Breaker.reset_tripped(pid)
+      breaker = Agent.get(pid, & &1)
+      refute breaker.tripped
+      assert breaker.break_count == 0
+    end
+
+    test "returns the breaker if it is not tripped", %{breaker: pid} do
+      Breaker.increment(pid, 2)
+      Breaker.reset_tripped(pid)
+      breaker = Agent.get(pid, & &1)
+      assert breaker.break_count == 1
     end
   end
 end
--- test/ex_break/registry_test.exs
+++ test/ex_break/registry_test.exs
@@ -4,6 +4,18 @@ defmodule ExBreak.RegistryTest do
 
   alias ExBreak.Registry
 
+  describe ".find_breaker/1" do
+    test "finds an existing breaker" do
+      func = fn -> :ok end
+      {:ok, pid} = Registry.get_breaker(func)
+      assert Registry.find_breaker(func) == {:ok, pid}
+    end
+
+    test "returns an error when the breaker does not exist" do
+      assert Registry.find_breaker(fn -> :ok end) == :error
+    end
+  end
+
   describe ".get_breaker/1" do
     test "registers a new breaker" do
       func = fn -> :ok end
